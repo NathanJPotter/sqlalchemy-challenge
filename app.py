89c8221ea8d4173155ec3590ad947357688bffe6
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-18 10:00:12 +1000

    Update app.py
    I wrote script to import libraries and set up the database.

---
+++ app.py
@@ -0,0 +1,32 @@
+# Import libraries
+import datetime as dt
+import numpy as np 
+import pandas as pd 
+
+import sqlaclchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from aqlalchemy import inspect, create_engine, func
+
+from flask import Flask, jsonify
+
+#############################################
+# Database Setup
+#############################################
+
+# Create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
